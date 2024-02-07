2568a95eb151b254c93c9d64a232d9da7db76137
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-02-07 20:44:34 +0200

    added a base controller

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Server.Requests;
+using Server.Responses;
+using Server.Exceptions;
+
+namespace Server.Controllers
+{
+[Controller]
+public abstract class BaseController : ControllerBase
+{
+    protected async Task<ActionResult> Try(Func<Task<BaseRequest>> func)
+    {
+        try
+        {
+            return MakeOk(await func());
+        }
+        catch (BaseException exception)
+        {
+            return MakeResponse(exception.Error);
+        }
+        catch (Exception exception)
+        {
+            return MakeResponse(new(HttpStatusCode.InternalServerError, exception.Message));
+        }
+    }
+
+    private OkObjectResult MakeOk(object data) => Ok(data);
+
+    private ObjectResult MakeResponse(ErrorResponse error)
+    {
+        return error.Code switch {
+            HttpStatusCode.BadRequest => MakeBadRequest(error.Message),
+            HttpStatusCode.Unauthorized => Unauthorized(error),
+            HttpStatusCode.NotFound => NotFound(error),
+            HttpStatusCode.InternalServerError =>
+                new ObjectResult(error) { StatusCode = StatusCodes.Status500InternalServerError },
+            _ => MakeBadRequest("IDK Brah..."),
+        };
+    }
+
+    private BadRequestObjectResult MakeBadRequest(string message) =>
+        BadRequest(new ErrorResponse(HttpStatusCode.BadRequest, message));
+}
+}
