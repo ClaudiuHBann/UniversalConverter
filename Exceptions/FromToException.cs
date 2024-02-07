7602cf84454618981293510b7f4e93d60b884431
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-02-07 20:35:25 +0200

    added some specific exceptions

---
+++ Exceptions/FromToException.cs
@@ -0,0 +1,24 @@
+﻿using Server.Services;
+
+namespace Server.Exceptions
+{
+public class FromToException : BaseException
+{
+    public FromToException(IService service, bool fromIsInvalid) : base(Format(service, fromIsInvalid))
+    {
+    }
+
+    public FromToException(IService service, bool fromIsInvalid, Exception inner)
+        : base(Format(service, fromIsInvalid), inner)
+    {
+    }
+
+    private static string Format(IService service, bool fromIsInvalid)
+    {
+        var serviceName = service.ToString()!.Replace("Service", "");
+        var fromOrTo = fromIsInvalid ? "from" : "to";
+
+        return $"{serviceName}'s {fromOrTo} is invalid!";
+    }
+}
+}
---
+++ Exceptions/ValueException.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.Exceptions
+{
+public class ValueException : BaseException
+{
+    public ValueException(string value) : base(value)
+    {
+    }
+
+    public ValueException(string value, Exception inner) : base(value, inner)
+    {
+    }
+}
+}
