ea8aa7d1c5cd76e07fa60649c54e9720572b0500
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-02-09 16:13:49 +0200

    added the default EUR currency and synced the rates updating

--- API/Services/CurrencyService.cs
+++ API/Services/CurrencyService.cs
@@ -12,12 +12,15 @@ public class CurrencyService : BaseService<CurrencyRequest, CurrencyResponse>
 {
     private const string Source = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
 
+    // the rates doesnt need a mutex because the _ratesLastUpdate acts like it
     private static Dictionary<string, decimal> _rates = [];
+
     private static DateTime _ratesLastUpdate = DateTime.MinValue;
+    private static readonly Mutex _mutexRatesLastUpdate = new();
 
     public override async Task<List<string>> FromTo()
     {
-        await UpdateRates();
+        await FindRates();
         return _rates.Select(rate => rate.Key).ToList();
     }
 
@@ -40,33 +43,34 @@ public class CurrencyService : BaseService<CurrencyRequest, CurrencyResponse>
     {
         await Validate(request);
 
-        await UpdateRates();
+        await FindRates();
         try
         {
+            // divide by from first because the currency is in EUR by default
             return new(request.Money.Select(money => money / _rates[request.From] * _rates[request.To]).ToList());
         }
         catch (OverflowException)
         {
             throw new ValueException($"The value converted from {request.From} to {request.To} is too small/big!");
         }
-        catch (Exception e)
-        {
-            throw new ValueException($"The value converted from {request.From} to {request.To} is too small/big!");
-        }
     }
 
-    private bool ShouldUpdateRates()
+    private static bool ShouldUpdateRates()
     {
+        _mutexRatesLastUpdate.WaitOne();
         if ((DateTime.UtcNow - _ratesLastUpdate).Days < 1)
         {
+            _mutexRatesLastUpdate.ReleaseMutex();
             return false;
         }
 
         _ratesLastUpdate = DateTime.UtcNow;
+
+        _mutexRatesLastUpdate.ReleaseMutex();
         return true;
     }
 
-    private async Task UpdateRates()
+    private static async Task FindRates()
     {
         if (!ShouldUpdateRates())
         {
@@ -83,6 +87,7 @@ public class CurrencyService : BaseService<CurrencyRequest, CurrencyResponse>
 
         _rates = xml.XPathSelectElements("//ecb:Cube[@currency and @rate]", nsManager)
                      .Select(cube => (cube.Attribute("currency")!.Value, decimal.Parse(cube.Attribute("rate")!.Value)))
+                     .Append(("EUR", 1m))
                      .ToDictionary();
     }
 }
