f28a01a879d0de96d331349c5278d8463080a126
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-02-10 17:26:33 +0200

    now we accept query props and the main page changes the current converter title

--- Web/src/components/Main.tsx
+++ Web/src/components/Main.tsx
@@ -1,15 +1,39 @@
 import { Stack, Text } from "@mantine/core";
-
 import Input from "./io/Input";
 import Output from "./io/Output";
 import Actions from "./Actions";
 import AppShellEx from "./app-shell/AppShellEx";
+import { useLocation } from "react-router-dom";
+import { UCContext } from "../contexts/UCContext.tsx";
+import { useContext } from "react";
+
+function FindCategoryHeader(category: string | null) {
+  var text = "Choose a category...";
+  if (!category) {
+    return text;
+  }
+
+  category = category.toLowerCase();
+  category = category[0].toUpperCase() + category.slice(1);
+
+  const context = useContext(UCContext);
+  if (context.categories.includes(category)) {
+    text = `${category} Converter`;
+  }
+
+  return text;
+}
 
 function App() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const category = searchParams.get("category");
+
   return (
     <AppShellEx>
       <Stack>
-        <Text>Choose a category...</Text>
+        <Text>{FindCategoryHeader(category)}</Text>
 
         <Input />
         <Actions />
