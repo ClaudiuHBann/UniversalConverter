3be594a5501ff6e5a8c0806d4352bf1aa940cea2
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-02-15 17:00:14 +0200

    added user secrets

---
+++ API/Entities/UCContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities
+{
+public class UCContext : DbContext
+{
+    private readonly string _connectionString = "";
+
+    public UCContext(IConfiguration config)
+    {
+        _connectionString = config["ConnectionStringUC"] ?? "";
+    }
+
+    public UCContext(DbContextOptions<UCContext> options) : base(options)
+    {
+    }
+
+    public DbSet<LinkEntity> Links { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseNpgsql(_connectionString);
+}
+}
