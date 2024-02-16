72d32a8581547940a300cd396ee6436aaf3bd309
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-02-16 12:55:00 +0200

    added tests for the link zip endpoint

--- Test/UCUnitTests/LinkZipUCUnitTest.cs
+++ Test/UCUnitTests/LinkZipUCUnitTest.cs
@@ -6,19 +6,29 @@ namespace Test.UCUnitTests
 [TestClass]
 public class LinkZipUCUnitTest : BaseUCUnitTest
 {
-    // TODO: add tests for LinkZipUC
+    private const string _urlRR = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    private const string _codeRR = "1";
+
     private readonly UCService _uc = new();
 
     // clang-format off
     public static LinkZipRequest LinkZipRequestValid { get; } = new()
     {
-        From = "",
-        To = "",
-        URLs = []
+        From = "Longifier",
+        To = "Shortifier",
+        URLs = [_urlRR]
     };
 
     public static IEnumerable<object[]> LinkZipInput { get; } = [
-        [true, new List<string>() { "", "" }, LinkZipRequestValid],
+        [true, new List<string>() { _codeRR }, LinkZipRequestValid],
+        [true, new List<string>() { _urlRR }, new LinkZipRequest(LinkZipRequestValid) { From = "Shortifier", To = "Longifier", URLs = [_codeRR] }],
+        [false, new List<string>(), new LinkZipRequest(LinkZipRequestValid) { URLs = Enumerable.Range(0, 70).Select(i => i.ToString()).ToList() }], // more than the maximum number of links
+        [false, new List<string>(), new LinkZipRequest(LinkZipRequestValid) { URLs = ["https://www.ma-ta.e.pe.mine.7-cutite/?"] }], // invalid link
+        [false, new List<string>(), new LinkZipRequest(LinkZipRequestValid) { To = "XXX" }], // invalid To
+        [false, new List<string>(), new LinkZipRequest(LinkZipRequestValid) { From = "XXX" }], // invalid From
+        [false, new List<string>(), new LinkZipRequest(LinkZipRequestValid) { From = "Longifier", To = "Longifier" }], // valid values but the same
+        [false, new List<string>(), new LinkZipRequest(LinkZipRequestValid) { From = "XXX", To = "XXX" }], // invalid From and To but can be skipped because the same
+        [false, new List<string>(), new LinkZipRequest(LinkZipRequestValid) { URLs = [new string('a', 2048)] }], // overflow
     ];
     // clang-format on
 
