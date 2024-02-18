6da371c21aa2919a22c8f85bdb179a902d4da51d
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-02-18 02:45:03 +0200

    fixed invoke extension object method added tests for common endpoint

--- Shared/Utilities/ObjectExtensions.cs
+++ Shared/Utilities/ObjectExtensions.cs
@@ -1,10 +1,20 @@
-﻿namespace Shared.Utilities
+﻿using System.Reflection;
+
+namespace Shared.Utilities
 {
 public static class ObjectExtensions
 {
     public static Result Invoke<Result>(this object obj, string methodName, object?[]? parameters = null)
     {
-        var method = obj.GetType().GetMethod(methodName);
+        var method = obj.GetType().GetMethod(
+            methodName, BindingFlags.Default | BindingFlags.IgnoreCase | BindingFlags.DeclaredOnly |
+                            BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic |
+                            BindingFlags.FlattenHierarchy | BindingFlags.InvokeMethod | BindingFlags.CreateInstance |
+                            BindingFlags.GetField | BindingFlags.SetField | BindingFlags.GetProperty |
+                            BindingFlags.SetProperty | BindingFlags.PutDispProperty | BindingFlags.PutRefDispProperty |
+                            BindingFlags.ExactBinding | BindingFlags.SuppressChangeType |
+                            BindingFlags.OptionalParamBinding | BindingFlags.IgnoreReturn |
+                            BindingFlags.DoNotWrapExceptions);
         var methodResult = method!.Invoke(obj, parameters)!;
         return (Result)methodResult;
     }
