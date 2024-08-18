4dbb643b3fbd70e3ae50fb4ca9bf0f243ca7bdf5
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-08-18 16:02:24 +0300

    added rankine scale for the temperature convertor

--- API/Services/TemperatureService.cs
+++ API/Services/TemperatureService.cs
@@ -9,16 +9,24 @@ namespace API.Services
 {
 public class TemperatureService : BaseService<TemperatureRequest, TemperatureResponse>
 {
+    // Base temperature scale is Celsius
     private static readonly Dictionary<string, Func<double, double>> _temperatureDirectConversions =
         new() { { "Celsius->Celsius", temperature => temperature },
                 { "Celsius->Fahrenheit", ToFahrenheit },
                 { "Celsius->Kelvin", ToKelvin },
+                { "Celsius->Rankine", ToRankine },
                 { "Fahrenheit->Fahrenheit", temperature => temperature },
                 { "Fahrenheit->Celsius", FromFahrenheit },
                 { "Fahrenheit->Kelvin", temperature => ToKelvin(FromFahrenheit(temperature)) },
+                { "Fahrenheit->Rankine", temperature => ToRankine(FromFahrenheit(temperature)) },
                 { "Kelvin->Celsius", FromKelvin },
                 { "Kelvin->Fahrenheit", temperature => ToFahrenheit(FromKelvin(temperature)) },
-                { "Kelvin->Kelvin", temperature => temperature } };
+                { "Kelvin->Kelvin", temperature => temperature },
+                { "Kelvin->Rankine", temperature => ToRankine(FromKelvin(temperature)) },
+                { "Rankine->Rankine", temperature => temperature },
+                { "Rankine->Celsius", FromRankine },
+                { "Rankine->Fahrenheit", temperature => ToFahrenheit(FromRankine(temperature)) },
+                { "Rankine->Kelvin", temperature => ToKelvin(FromRankine(temperature)) } };
 
     private static readonly List<string> _fromTo =
         _temperatureDirectConversions.Select(tdc => tdc.Key.Split("->").First()).Distinct().ToList();
@@ -59,6 +67,8 @@ public class TemperatureService : BaseService<TemperatureRequest, TemperatureRes
     }
 
     private static double ToKelvin(double temperature) => temperature + 273.15;
+    private static double ToRankine(double temperature) => temperature * 1.8 + 491.67;
+    private static double FromRankine(double temperature) => (temperature - 491.67) * 0.555555556;
     private static double FromKelvin(double temperature) => temperature - 273.15;
     private static double ToFahrenheit(double temperature) => temperature * 1.8 + 32.0;
     private static double FromFahrenheit(double temperature) => (temperature - 32.0) * 0.555555556;
