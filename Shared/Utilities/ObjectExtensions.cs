f563cd7c1f6a37d02529d35eda4d5261b251a854
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-08-24 21:37:37 +0300

    improved invoke method for objects

--- Shared/Utilities/ObjectExtensions.cs
+++ Shared/Utilities/ObjectExtensions.cs
@@ -6,19 +6,40 @@ public static class ObjectExtensions
 {
     public static Result Invoke<Result>(this object obj, string methodName, object?[]? parameters = null)
     {
-        // TODO: refactor this because is very sensitive
-
-        var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public |
-                           BindingFlags.FlattenHierarchy | BindingFlags.InvokeMethod;
-        var method = obj.GetType().GetMethod(methodName, bindingFlags);
-        if (method!.IsGenericMethod)
+        ArgumentNullException.ThrowIfNull(obj);
+        if (string.IsNullOrWhiteSpace(methodName))
         {
-            var parametersTypes = parameters != null ? parameters.Select(param => param!.GetType()).ToArray() : [];
-            method = method.MakeGenericMethod(parametersTypes);
+            throw new ArgumentException($"Method name '{methodName}' is not valid!");
         }
 
-        var methodResult = method.Invoke(obj, parameters)!;
-        return (Result)methodResult;
+        // (non)public non(static) member from hierarchy
+        var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public |
+                           BindingFlags.FlattenHierarchy;
+
+        var method = obj.GetType().GetMethod(methodName, bindingFlags) ??
+                     throw new MissingMethodException(
+                         $"Method '{methodName}' with type '{obj.GetType().FullName}' doesn't exist!");
+
+        if (method.IsGenericMethod)
+        {
+            var parameterTypes = parameters != null ? parameters.Select(param => param!.GetType()).ToArray() : [];
+            method = method.MakeGenericMethod(parameterTypes);
+        }
+
+        try
+        {
+            var result = method.Invoke(obj, parameters);
+            if (typeof(Result).IsValueType)
+            {
+                ArgumentNullException.ThrowIfNull(result);
+            }
+
+            return (Result)result!;
+        }
+        catch (TargetInvocationException ex)
+        {
+            throw ex.InnerException ?? ex;
+        }
     }
 
     public static bool Equal(this object obj1, object obj2) => obj1.ToJSON() == obj2.ToJSON();
