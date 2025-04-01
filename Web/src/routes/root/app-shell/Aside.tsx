a1883da6d0adbce6a0345c2863561ada59b50d04
Claudiu HBann <123uidualcnnamreh@gmail.com>
2025-04-02 00:37:29 +0300

    added Logs title

--- Web/src/routes/root/app-shell/Aside.tsx
+++ Web/src/routes/root/app-shell/Aside.tsx
@@ -1,12 +1,16 @@
-import { Stack, ScrollArea, Notification } from "@mantine/core";
-import { useUCContext } from "../../../contexts/UCContext";
+import { Center, Notification, rem, ScrollArea, Stack, Text } from "@mantine/core";
 import { IconX } from "@tabler/icons-react";
+import { useUCContext } from "../../../contexts/UCContext";
 
 function Aside() {
   const context = useUCContext();
 
   return (
-    <ScrollArea m={10} offsetScrollbars>
+    <ScrollArea m={10} offsetScrollbars="y">
+      <Center mb={10}>
+        <Text size={rem(18)}>Logs</Text>
+      </Center>
+
       <Stack align="stretch" justify="flex-start" gap="xs">
         {context!.GetLogs().map((log, index) => {
           return (
